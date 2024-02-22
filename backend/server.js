c99fc034903c83d388bdffc1ad3fac11ec79d4de
Kishen Ranasinghe <kishen@codegen.net>
2024-02-22 23:08:49 +0530

    server.js

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const item = require('./models/itemModel')
+const Admin = require('./models/adminModel')
+
+
+
+const app = express();
+const db = require('./db')
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+
+const itemRoute = require('./routes/itemsRoute')
+const adminRoute = require('./routes/adminRoute')
+
+
+app.use('/api/items/', itemRoute)
+app.use('/api/admins/', adminRoute)
+
+
+app.get("/", (req, res) => {
+
+    res.send("Server Working!");
+
+});
+
+
+const port = process.env.PORT || 8070;
+
+app.listen(port, () => `Server is up and running on port number: ${port}`);
